ec1c6f7c551a1191f2a74ae1e14ef03b2221c122
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-25 22:50:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// var, let and const
+var age = 20;
+var age = 23;
+// we can use same name multiple using var.
+
+let price = 23;
+price = 30;
+// let price = 45;  we cannot declare same name multiple using let, it shows an error.
+console.log(price)
+
+const number = 34;
+// number = 35; we cannot update same name value using const.
+console.log(number);
+
+

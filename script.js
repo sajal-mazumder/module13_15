90a8465fb76bda304542e1d5196c87907db0886b
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-25 23:35:37 +0600

    some update

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 // var, let and const
-var age = 20;
-var age = 23;
+// var age = 20;
+// var age = 23;
 // we can use same name multiple using var.
 
-let price = 23;
-price = 30;
+// let price = 23;
+// price = 30;
 // let price = 45;  we cannot declare same name multiple using let, it shows an error.
-console.log(price)
+// console.log(price)
 
-const number = 34;
+// const number = 34;
 // number = 35; we cannot update same name value using const.
-console.log(number);
+// console.log(number);
 
 // data type 1. primitive data: number, string, boolean, integer, float
-let num = 23;
-let sTring = 'sajal';
-let flOat = 23.5;
-let boolEan = true;
-let Int = 25;
+// let num = 23;
+// let sTring = 'sajal';
+// let flOat = 23.5;
+// let boolEan = true;
+// let Int = 25;
 // convert float number to integer
-let Float = 23.4;
+// let Float = 23.4;
 // let inte = parseInt(Float); technique 01
 // console.log(inte)
 // console.log(parseInt.Float) not working
 
-let intg = 23; // 
-let flOAt = parseFloat(intg); // convert integer to float 
-console.log(flOAt)
+// let intg = 23; // 
+// let flOAt = parseFloat(intg); // convert integer to float 
+// console.log(flOAt)
 
 // checking type of
-let num1 = 12;
-console.log( typeof num1);
-let name = 'sajal';
-console.log( typeof name);
-let isHappy = true;
-console.log( typeof isHappy);
-let num2 = 34.5;
-console.log( typeof num2)
+// let num1 = 12;
+// console.log( typeof num1);
+// let name = 'sajal';
+// console.log( typeof name);
+// let isHappy = true;
+// console.log( typeof isHappy);
+// let num2 = 34.5;
+// console.log( typeof num2)
 
+// convert num to string and string to num
+let num3 = 20;
+let newNum = toString.num3;
+console.log(newNum)
+
+let num4 = '23';
+let num44 = parseInt(num4);
+console.log(num44);
+
+let namee = 'sajal';
+namee = parseInt(namee);
+console.log(namee);
 
 
 

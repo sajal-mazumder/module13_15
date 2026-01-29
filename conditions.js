e5ee079fed5e5a1e4822e77b47ca8e28c2d1acc5
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-29 19:32:14 +0600

    some update

--- conditions.js
+++ conditions.js
@@ -30,24 +30,47 @@
 
 // else if condition
 
-let price = 2000;
+// let price = 2000;
 
-if (price > 5000) {
-    // 10% discount
-    let discount = price * 10/100;
-    let payAmount = price - discount;
+// if (price > 5000) {
+//     // 10% discount
+//     let discount = price * 10/100;
+//     let payAmount = price - discount;
+//     console.log(payAmount);
+// }
+// else if( price > 2500){
+//     // discount 5% 
+//     let discount = price * 5/100;
+//     let payAmount = price - discount;
+//     console.log(payAmount)
+// }
+// else if(price > 1500){
+//     // a scratch card
+//     console.log('scratch your card and have a gift.')
+// }
+// else{
+//     console.log(price)
+// }
+
+let age = 25;
+let foodPrice = 300;
+
+if( age <= 12){
+    // full free
+    console.log('you can eat free of cost')
+}
+else if(age > 40){
+    // 5% discount
+    let discount = foodPrice * 5/100;
+    let payAmount = foodPrice - discount;
     console.log(payAmount);
 }
-else if( price > 2500){
-    // discount 5% 
-    let discount = price * 5/100;
-    let payAmount = price - discount;
-    console.log(payAmount)
-}
-else if(price > 1500){
-    // a scratch card
-    console.log('scratch your card and have a gift.')
+else if(age > 60){
+    // 50% discount
+    let discount = foodPrice *50/100;
+    let payAmount = foodPrice - discount;
+    console.log(payAmount);
 }
 else{
-    console.log(price)
+    console.log("you are requested to pay : ", foodPrice)
 }

2794cae577ef906d4310f04f1486970a92242534
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-29 23:19:04 +0600

    some update

---
+++ array.js
@@ -0,0 +1,9 @@
+
+
+let arr = [1, 2, 3, 4, 5, 43, 32, 34, 54, 43, 45, 53, 44,77]
+console.log(arr);
+console.log(arr.length);
+console.log(arr[3])
+
+arr[5] = 390;
+console.log(arr)
--- task14.js
+++ task14.js
@@ -11,20 +11,59 @@
 
 // bmi calculator
 
-let weight = 95;
-let height = 1.5;
-let bmi = weight / height ** 2;
+// let weight = 95;
+// let height = 1.5;
+// let bmi = weight / height ** 2;
 
-if (bmi < 18.5) {
-    console.log('you are under weight. take healthy food to improve your physical condition.')
-}else if(bmi == 18.5 && bmi <= 24.9)
-    { console.log('you are normal in weight and height value and you are ok.') 
-}else if (bmi > 25 && bmi <= 29) {
-    console.log('you are overweight. control your weight.')
-}else if (bmi > 29 && bmi == 39) {
-    console.log('you are obese. you should strictly follow the hygene rule and must control your weight.')
-}else if (bmi > 39) {
-    console.log('your weight is out or control. sympathy for you.')
+// if (bmi < 18.5) {
+//     console.log('you are under weight. take healthy food to improve your physical condition.')
+// }else if(bmi == 18.5 && bmi <= 24.9)
+//     { console.log('you are normal in weight and height value and you are ok.') 
+// }else if (bmi > 25 && bmi <= 29) {
+//     console.log('you are overweight. control your weight.')
+// }else if (bmi > 29 && bmi == 39) {
+//     console.log('you are obese. you should strictly follow the hygene rule and must control your weight.')
+// }else if (bmi > 39) {
+//     console.log('your weight is out or control. sympathy for you.')
+// }else{
+//     console.log('thank you for calculating bmi.')
+// }
+
+// grading calculator
+
+// let marks = 28;
+// if (marks >= 80) {
+//     console.log('congratulations! you obtained A+.')
+// }else if (marks >= 70 && marks <= 79) {
+//     console.log('congratulations! you obtained gpa over 4 out of 5. keep it up.')
+// }else if (marks >= 60 && marks <= 69) {
+//     console.log('your result is gpa 3.5 out of 5. keep cool')
+// }
+// else if (marks >= 50 && marks <= 59) {
+//     console.log('your result is gpa 3 out of 5.')
+// }
+// else if (marks >= 40 && marks <= 49) {
+//     console.log('your result is gpa 2 out of 5.')
+// }
+// else if (marks >= 33 && marks <= 39) {
+//     console.log( 'your result is gpa 1 out of 5. very poor result!')
+// }else{
+//     console.log('Ops! you are failed. ')
+// }
+
+
+//  nested friend 
+
+let myScore = 85;
+let friendScore = 66;
+
+if (myScore >= 80 && friendScore >= 75) {
+    console.log('let us having lunch in a three star restaurant.')
+    if (friendScore <= 60) {
+        console.log('bill will pay my friend.')
+    }else if (friendScore <= 50) {
+        console.log('no meeting with my friend.')
+    }
 }else{
-    console.log('thank you for calculating bmi.')
+    console.log('block! no connection with you.')
 }

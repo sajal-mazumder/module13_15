c1a3720068f0b1ea59ea85d78f5664d89163d1e4
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-29 19:53:51 +0600

    some update

---
+++ nestedConditions.js
@@ -0,0 +1,17 @@
+
+
+let money = 50;
+
+if (money > 350) {
+    console.log("you are enough to ride a car")
+}else{
+    if (money > 200) {
+        console.log('you are reliable to enter a cineplex')
+    }else{
+        if (money > 100) {
+            console.log('go to a free entry park')
+        }else{
+            console.log('stay on home')
+        }
+    }
+}

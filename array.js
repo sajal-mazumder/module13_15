2147e43cf26dd179c33989e8ab23f18e56263b65
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-29 23:50:49 +0600

    some update

--- array.js
+++ array.js
@@ -1,9 +1,37 @@
 
 
 let arr = [1, 2, 3, 4, 5, 43, 32, 34, 54, 43, 45, 53, 44,77]
-console.log(arr);
-console.log(arr.length);
-console.log(arr[3])
+// console.log(arr);
+// console.log(arr.length); // check the length of array by its total element numbers
+// console.log(arr[3])
 
-arr[5] = 390;
-console.log(arr)
+// arr[5] = 390;
+// console.log(arr) // update an element by its index number
+
+// arr.pop();
+// console.log(arr); // delete last element from an array
+
+// arr.push(908);
+// console.log(arr); // insert an element to last 
+
+// arr.shift();
+// console.log(arr); // remove an element from 0 index
+
+// arr.unshift(8907);
+// console.log(arr) // insert an element to the 0 index
+
+// console.log(arr.includes(56)); // checking if element is present or not in the relevant array
+// console.log(arr.indexOf(390)); // find the index number of an element by its name
+// console.log(arr.indexOf(1234)) // result is -1, bcz it is not exists.
+
+
+let arr2 = [1, 2, 3, 4, 5, 43, 32, 34, 54, 43, 45, 53, 44,77]
+let arr3 = [];
+let num = 134;
+let arr4 = [3, 5, 66, 4, 443, 447]
+// console.log(Array.isArray(arr2)); // checking is variable is array or not
+// console.log(Array.isArray(arr3)); // checking is variable is array or not
+// console.log(Array.isArray(num)); // checking is variable is array or not
+
+let concat = arr2.concat(arr4);
+console.log(concat);

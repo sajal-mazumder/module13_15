17e6e26daf52a3476690f82632fdb4709f6f9875
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-30 09:56:40 +0600

    some update

--- loop.js
+++ loop.js
@@ -1,15 +1,15 @@
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,  90, 87,  76,  65, 54, 43, 32, 21];
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,  90, 87,  76,  65, 54, 43, 32, 21];
 // for of loop
-for (number of numbers) {
-    console.log(number)
-} // this loop is active
+// for (number of numbers) {
+//     console.log(number)
+// } // this loop is active
 
-// for loop
-for (let index = 0; index < numbers.length; index++) {
-    // const element = array[index];
-    console.log(index)
-} // this loop is also active
+// // for loop
+// for (let index = 0; index < numbers.length; index++) {
+//     // const element = array[index];
+//     console.log(index)
+// } // this loop is also active
 
 // while loop
 
@@ -20,8 +20,40 @@ for (let index = 0; index < numbers.length; index++) {
 //     numbers = numbers + 1;
 // }
 
-let num = 1;
-while (num < 10) {
-    console.log('result is printing');
-    num++;
+// let num = 1;
+// while (num < 10) {
+//     console.log('result is printing');
+//     num++;
+// }
+
+// let num1 = 1;
+// let sum = 0;
+
+// while (num1 <= 10) {
+//     console.log(num1);
+//     // num1++; // it shows one type of output
+//     sum = sum + num1;
+//     console.log('sum of numbers is :', sum);
+//     num1 += 1; // it shows another output
+// }
+
+// let num2 = 2; // this code result shows true or false
+// let even = num2 % 2 == 0;
+// while (num2 <= 20) {
+//     console.log(even);
+//     num2++;
+// }
+
+let num3 = 1;
+
+while (num3 <= 20) {
+    // console.log(num3);
+    num3++;
+    // if (num3% 2 === 0) { // finding even numbers
+    //     console.log('even number is : ', num3); 
+    // } 
+
+    // if (num3 % 2 === 1) { // finding odd numbers
+    //     console.log('odd number is : ', num3);
+    // }
 }

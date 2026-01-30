62d9b62188f57668e5ec74c2ee018c16870c234c
sajal-mazumder <sajalmazumder47@gmail.com>
2026-01-30 20:41:23 +0600

    some update

---
+++ breakContinue.js
@@ -0,0 +1,74 @@
+
+
+// break condition
+
+// for( let i = 0; i <= 20; i++){
+//     // console.log(i); // shows result upto 13
+//     if (i > 12) {
+//         break;
+//     }
+//     // console.log(i); // shows result upto 12
+// }
+// console.log('break up with u!'); // when break is on fire this line execute
+
+// while loop
+
+// let i = 0;
+// while (i <= 20) {
+//     // console.log(i); // result upto 13
+//     if (i > 12) {
+//         // console.log(i); // result above from 12
+//         break;
+//         // console.log(i); // no result
+//     }
+//     // console.log(i); // result upto 12
+//     i++;
+// }
+
+
+// continue
+
+// for loop
+
+// for( let i = 0; i <= 20; i++){
+//     // if (i < 10) { // result starts from 10
+//     //     continue;
+//     // }
+//     // if (i > 10) { // result upto 10
+//     //     continue;
+//     // }
+//     // console.log(i);
+
+//     // if (i % 3 == 0) {
+//     //     console.log(i); // result 0, 3, 6 etc
+//     // }
+
+//     if (i % 3 !== 0) {
+//         console.log(i); // result avoid 3, 6, 9, 12 etc
+//     }
+// }
+
+
+
+
+// do while loop 
+// at least one time run the code 
+
+
+// let n = 0;
+// do{
+//     console.log(n);
+//     n++;
+// }
+// while (n <= 5) {
+//     console.log(n); // result upto 6
+// }
+
+let m = 10;
+do{
+    console.log(m);
+    m++;
+}
+while (m <= 5) {
+    console.log(m);
+}
